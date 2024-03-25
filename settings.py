2710cc7073f03871b17693b3391119a105af4e72
Tec3202 <118991213+Tec3202@users.noreply.github.com>
2024-03-25 16:54:15 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -30,9 +30,9 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
-VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
-VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
-VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
+VIDEO_1_PATH = ''
+VIDEO_2_PATH = ''
+VIDEO_3_PATH = ''
 VIDEOS_DICT = {
     'video_1': VIDEO_1_PATH,
     'video_2': VIDEO_2_PATH,
